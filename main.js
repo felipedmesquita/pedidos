8cb747f905c354239972cee5fa761e2be6d0c462
sarahgodoyy <104001680+sarahgodoyy@users.noreply.github.com>
2022-04-22 09:19:05 -0300

    adicionei cep

--- main.js
+++ main.js
@@ -1,41 +1,54 @@
-
 async function getID() {
+
     const params = new URLSearchParams(window.location.search)
     key = params.get('key')
     const cpf = prompt("Insira o CPF ou CNPJ ");
-    res = await fetch("https://bling.servidorjulia.workers.dev/Api/v2/contato/" + cpf + "/json?apikey="+key);
-    json = await res.json();
+    res = await fetch(`https://bling.servidorjulia.workers.dev/Api/v2/contato/${cpf}/json?apikey=${key}`);
+    json1 = await res.json();
 
-    const id = json.retorno.contatos[0].contato.id;
+    const id = json1.retorno.contatos[0].contato.id;
 
     console.log(id);
 
     resp = await fetch(`https://bling.servidorjulia.workers.dev/Api/v2/pedidos/json?filters=idContato[${id}]&apikey=${key}`);
-    json2 = await resp.json();
+    json = await resp.json();
 
-    pedidos = json2.retorno.pedidos;
+    pedidos = json.retorno.pedidos;
 
     var d1 = document.getElementById('tabela_tail');
 
     for (p of pedidos) {
         itens = p.pedido.itens;
         for(i of itens){
+
             const num = p.pedido.numero;
             const item = i.item.descricao;
             const qtd = i.item.quantidade;
             const preco = i.item.valorunidade;
+            const idloja = p.pedido.loja;
+            // const nomeloja = p.pedido.loja;
+            const cep = p.pedido.cliente.cep;
             const data = p.pedido.data;
             const sit = p.pedido.situacao;
-            
+           
+            // if(loja = idloja){
+            //     title = loja;
+            //     nomeLoja = nomeloja;
+            // }
+            const loja = p.pedido.loja;
+            const nomeloja = p.pedido.loja;
+
             template_tailwind = `<tr>
             <td class="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-6">${num}</td>
             <td class="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">${item}</td>
             <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-900">${parseInt(qtd).toFixed(2)}</td>
             <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">${parseFloat(preco).toFixed(2)}</td>
+            <!--<td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">${loja}</td>
+            <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">${nomeloja}</td>-->
+            <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">${cep}</td>
             <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">${data}</td>
             <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">${sit}</td>
             </tr>`
-
 
             d1.insertAdjacentHTML('beforeend', template_tailwind);
         }
@@ -45,12 +58,3 @@ async function getID() {
 }
 
 getID();
-
-/*async function get(){
-    const cpf = prompt("Insira o CPF ou CNPJ ");
-    res = await fetch("https://bling.com.br/Api/v2/contato/"+ cpf + "/json?apikey=338ef3faa1da74d984ec8d87b864a732d01e70a69d9aedae90c1465427402991b76b2673");
-    json = await res.json();
-    
-    console.log(json.retorno.contatos[0].contato.id);
-}
-getID();*/
