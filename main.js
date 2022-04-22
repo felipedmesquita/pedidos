11b83ef9745d7cd6ce56e048e20c02be65823f3d
sarahgodoyy <104001680+sarahgodoyy@users.noreply.github.com>
2022-04-22 09:47:34 -0300

    icluso nomes das lojas

--- main.js
+++ main.js
@@ -25,26 +25,18 @@ async function getID() {
             const item = i.item.descricao;
             const qtd = i.item.quantidade;
             const preco = i.item.valorunidade;
-            const idloja = p.pedido.loja;
-            // const nomeloja = p.pedido.loja;
             const cep = p.pedido.cliente.cep;
             const data = p.pedido.data;
             const sit = p.pedido.situacao;
-           
-            // if(loja = idloja){
-            //     title = loja;
-            //     nomeLoja = nomeloja;
-            // }
-            const loja = p.pedido.loja;
-            const nomeloja = p.pedido.loja;
+        
+            const nomeloja = lojas.find(l => l.id == "203234101").nomeLoja;
 
             template_tailwind = `<tr>
             <td class="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-6">${num}</td>
             <td class="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">${item}</td>
             <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-900">${parseInt(qtd).toFixed(2)}</td>
             <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">${parseFloat(preco).toFixed(2)}</td>
-            <!--<td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">${loja}</td>
-            <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">${nomeloja}</td>-->
+            <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">${nomeloja}</td>
             <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">${cep}</td>
             <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">${data}</td>
             <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">${sit}</td>
@@ -56,5 +48,4 @@ async function getID() {
         
     }
 }
-
 getID();
