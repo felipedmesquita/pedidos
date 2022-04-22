a0c3244617a4b6ee13f7109f434a4859b2f1a241
sarahgodoyy <104001680+sarahgodoyy@users.noreply.github.com>
2022-04-22 10:49:47 -0300

    mudei o titulo
    agora tem endereco, cep, telefone etc

--- main.js
+++ main.js
@@ -7,15 +7,45 @@ async function getID() {
     json1 = await res.json();
 
     const id = json1.retorno.contatos[0].contato.id;
-
-    console.log(id);
+    const nomeCliente = json1.retorno.contatos[0].contato.nome;
+    const cpfcnpj = json1.retorno.contatos[0].contato.cnpj;
+    const telefoneCliente = json1.retorno.contatos[0].contato.fone;
+    const dataCliente = json1.retorno.contatos[0].contato.clienteDesde;
+    ///////////////////////////
+    const rua = json1.retorno.contatos[0].contato.endereco;
+    const num = json1.retorno.contatos[0].contato.numero;
+    const bairro = json1.retorno.contatos[0].contato.bairro;
+    const complemento = json1.retorno.contatos[0].contato.complemento;
+    const uf = json1.retorno.contatos[0].contato.uf;
+    const cep = json1.retorno.contatos[0].contato.cep;
+    ////////////////////////////
+    const endereco = `${rua}, ${num}, ${bairro} - ${uf}`;
 
     resp = await fetch(`https://bling.servidorjulia.workers.dev/Api/v2/pedidos/json?filters=idContato[${id}]&apikey=${key}`);
     json = await resp.json();
 
     pedidos = json.retorno.pedidos;
 
-    var d1 = document.getElementById('tabela_tail');
+    const tabela = document.getElementById('tabela_tail');
+    const titulo = document.getElementById('titulo');
+
+    template_titulo = `<tr>
+    <br>
+    <h1 class="text-xl font-semibold text-gray-900">PEDIDOS DO CLIENTE ${nomeCliente} </h1>
+    <p class="mt-2 text-sm text-gray-700">
+    CPF/CNPJ: ${cpfcnpj}
+    <br>
+    Endereço: ${endereco}
+    <br>
+    CEP: ${cep}
+    <br>
+    Complemento: ${complemento}
+    <br>
+    Telefone: ${telefoneCliente}
+    <br>
+    Cliente desde: ${dataCliente}
+    </p>
+    </tr>`
 
     for (p of pedidos) {
         itens = p.pedido.itens;
@@ -26,7 +56,6 @@ async function getID() {
             const qtd = i.item.quantidade;
             const preco = i.item.valorunidade;
             const loja = p.pedido.loja;
-            const cep = p.pedido.cliente.cep;
             const data = p.pedido.data;
             const sit = p.pedido.situacao;
         
@@ -38,15 +67,15 @@ async function getID() {
             <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-900">${parseInt(qtd).toFixed(2)}</td>
             <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">${parseFloat(preco).toFixed(2)}</td>
             <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">${nomeloja}</td>
-            <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">${cep}</td>
             <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">${data}</td>
             <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">${sit}</td>
             </tr>`
 
-            d1.insertAdjacentHTML('beforeend', template_tailwind);
+            tabela.insertAdjacentHTML('beforeend', template_tailwind);
         }
 
         
     }
+    titulo.insertAdjacentHTML('beforeend', template_titulo);
 }
 getID();
